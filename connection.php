2c31d460f9f336e35ba0bf065915ab3eb53679de
Nessy <programmingwithnessy@gmail.com>
2025-04-26 12:18:04 +0800

    activity 1 and 2

--- connection.php
+++ connection.php
@@ -1,15 +1,14 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$dbname = "db_batch5_obms";
-// online book management system - obms
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$password = ""; // your MySQL password if applicable
+$database = "db_delatorre6_project";
 
-// Check connection
+$conn = new mysqli($servername, $username, $password, $database);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully <br/>";
+
+session_start();
 ?>
